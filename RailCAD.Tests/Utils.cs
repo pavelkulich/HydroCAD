43365c561e9afc05235fe836b6211ac66b7e4ea2
pavelkulich <p.kulich@yahoo.com>
2026-03-24 11:51:02 +0100

    Add files via upload

---
+++ RailCAD.Tests/Utils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RailCAD.Tests
+{
+    internal class Utils
+    {
+        internal static void AssertEqualsWithTol(double expected, double actual, string message = "", double tolerance = 1e-6)
+        {
+            Assert.IsTrue(Math.Abs(expected - actual) < tolerance, message);
+        }
+    }
+}
